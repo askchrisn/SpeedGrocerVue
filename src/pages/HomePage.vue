7eaf7513190e7a8e03562e37b3048fe42ffbe08d
AvoidDeath <89741500+AvoidDeath@users.noreply.github.com>
2023-12-28 15:54:14 -0500

    Added example grocery lists view

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,20 @@
 <template>
-  <label>HomePage.vue</label>
-  <label>{{ authStore.isAuthenticated }}</label>
-  <q-btn class="primary" to="/login" @click="authStore.logout()">Logout</q-btn>
-  <example-component/>
+  <div class="main">
+    <label>HomePage.vue</label>
+    <label>{{ authStore.isAuthenticated }}</label>
+    <q-btn class="primary" to="/login" @click="authStore.logout()">Logout</q-btn>
+    <q-list bordered>
+        <q-item v-for="groceryList in groceryLists" :key="groceryList.id" class="q-my-sm" clickable v-ripple>
+          <q-item-section>
+            <q-item-label>{{ groceryList.name }}</q-item-label>
+          </q-item-section>
+
+          <q-item-section side>
+            <q-item-label caption lines="1">{{ groceryList.items.length }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -10,13 +22,33 @@
   import { useAuthStore } from 'src/stores/authStore';
 
   const authStore = useAuthStore();
+  const groceryLists = [ {
+    id: 1,
+    name: 'Home',
+    items: ['butter', 'milk', 'eggs'],
+    users: ['conno1234@gmail.com']
+  }, {
+    id: 2,
+    name: 'Work',
+    items: ['taco shell', 'meat'],
+    users: ['conno1234@gmail.com']
+  }, {
+    id: 3,
+    name: 'Other',
+    items: ['butter', 'milk', 'eggs', 'blarg', 'something'],
+    users: ['conno1234@gmail.com']
+  }, {
+    id: 4,
+    name: 'My List',
+    items: [],
+    users: ['conno1234@gmail.com']
+  } ]
 </script>
 
 <style scoped>
-  div {
-    margin: 6rem 2rem;
+  .main {
+    margin: 2rem;
   }
-
   .test {
     display: flex;
     flex-direction: column;
