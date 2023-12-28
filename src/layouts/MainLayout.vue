52ff5ae5e1379717ecf79237f833d730241f6f10
AvoidDeath <89741500+AvoidDeath@users.noreply.github.com>
2023-12-28 12:32:32 -0500

    Removed toolbar side menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,14 +2,6 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
 
         <q-toolbar-title>
           TiT
@@ -18,21 +10,6 @@
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
