e79ccaf9baa2cf931edd5a575778d7bd174696dc
AvoidDeath <89741500+AvoidDeath@users.noreply.github.com>
2023-12-28 14:59:26 -0500

    Added inputs to Login and SignUp pages

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,12 +1,20 @@
 <template>
     <label>LoginPage.vue</label>
     <div class="test">
-      <q-btn color="primary">Login</q-btn>
-      <q-btn color="primary" to="/signup">Sign Up</q-btn>
+        <q-input class="input" filled v-model="email" label="Email" stack-label dense></q-input>
+        <q-input class="input" filled v-model="password" label="Password" stack-label dense></q-input>
+        <q-btn color="primary">Login</q-btn>
+        <q-btn color="primary" to="/signup">Sign Up</q-btn>
     </div>
 </template>
   
 <script setup lang="ts">
+
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+
 </script>
   
 <style scoped>
@@ -17,6 +25,9 @@
     .test {
         display: flex;
         flex-direction: column;
+    }
+    .input {
+        margin-bottom: 2rem;
     }
     .q-btn {
         margin: 1rem;
--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -1,11 +1,23 @@
 <template>
     <label>SignUpPage.vue</label>
     <div class="test">
+        <q-input class="input" filled v-model="email" label="Email" stack-label dense></q-input>
+        <q-input class="input" filled v-model="name" label="Name" stack-label dense></q-input>
+        <q-input class="input" filled v-model="password" label="Password" stack-label dense></q-input>
+        <q-input class="input" filled v-model="passwordConfirmed" label="Confirm Password" stack-label dense></q-input>
       <q-btn color="primary">Create Account</q-btn>
     </div>
 </template>
   
 <script setup lang="ts">
+
+import { ref } from 'vue'
+
+const email = ref('')
+const name = ref('')
+const password = ref('')
+const passwordConfirmed = ref('')
+
 </script>
   
 <style scoped>
@@ -16,6 +28,9 @@
     .test {
         display: flex;
         flex-direction: column;
+    }
+    .input {
+        margin-bottom: 2rem;
     }
     .q-btn {
         margin: 1rem;
