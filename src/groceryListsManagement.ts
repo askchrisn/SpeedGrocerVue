c492318389f2f92f63a1e09ddaa3a33a1f9dbd75
askchrisn <fundasappteam@gmail.com>
2023-12-31 04:08:07 -0500

    Added groceryListManagement

---
+++ src/groceryListsManagement.ts
@@ -0,0 +1,36 @@
+import { ref } from "vue";
+import { attachEvent } from "./firebaseConfig";
+import GroceryList from "./models/groceryList";
+
+export const groceryLists = ref<Array<[string, GroceryList]>>([])
+
+export function getAUsersLists(email: string)
+{ 
+    return groceryLists.value.filter(([key, gl]) => gl.containsUser(email)); 
+}
+
+export function getAUsersFriendsHistory(email: string) {    
+    var usersLists = getAUsersLists(email);
+    let history = usersLists.flatMap(([, list]) => list.Users); // this gets all the users in the lists
+   
+    // remove duplicates and the user that asked for the list
+    let recommended: string[] = [];
+    for (let user of history) {
+        if (user !== email && !recommended.includes(user)) {
+            recommended.push(user);
+        }
+    }
+
+    return recommended;
+}
+
+const listener = attachEvent("GroceryLists", (snapshot) => {
+    var updatedGroceryLists: Array<[string, GroceryList]> = []
+
+    for (let key in snapshot) {
+        var gl = GroceryList.fromObject(snapshot[key])
+        updatedGroceryLists.push([key, gl])
+    }
+
+    groceryLists.value = updatedGroceryLists
+});
