9a08edddd1506a2c1a20c24e3e2c6d8bfbbe6156
askchrisn <fundasappteam@gmail.com>
2024-01-01 15:42:09 -0500

    Recommended Users

--- src/pages/UsersPage.vue
+++ src/pages/UsersPage.vue
@@ -11,7 +11,7 @@
       
         <q-virtual-scroll
             class="flex-grow"
-            :items="users"
+            :items="usersOfList"
             separator
             v-slot="{ item, index }"
             >
@@ -25,47 +25,89 @@
                 </q-item-section>
             </q-item>
         </q-virtual-scroll>
+
+        <q-virtual-scroll
+            class="flex-grow"
+            :items="recommendedUsers"
+            separator
+            v-slot="{ item, index }"
+            >
+            <q-item clickable v-ripple @click="addRecommendedUser(item.Email)">
+                <q-item-section>
+                    <q-item-label>{{ item.Nickname }}</q-item-label>
+                </q-item-section>
+
+                <q-item-section side>
+                    <q-item-label caption lines="1">{{ item.Email }}</q-item-label>
+                </q-item-section>
+            </q-item>
+        </q-virtual-scroll>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import Item from 'src/models/item';
-import { attachEvent, updateDb, setDb } from 'src/firebaseConfig'
+import { ref, computed } from 'vue';
+import { updateDb, setDb } from 'src/firebaseConfig'
 import { useAuthStore } from 'src/stores/authStore';
 import { useGroceryListKeyStore } from 'src/stores/groceryListKeyStore';
-import { useQuasar } from 'quasar'
-import GroceryList from 'src/models/groceryList';
 import UserInfo from 'src/models/userInfo';
 import { getUserInfo } from 'src/userManagement';
 import { useRouter } from 'vue-router';
+import { getAllPeopleOnList, getList, getAUsersLists } from 'src/groceryListsManagement';
+import { usePopupStore } from 'src/stores/popupStore';
 
 const router = useRouter();
 const authStore = useAuthStore()
-const quasar = useQuasar()
 const groceryListKeyStore = useGroceryListKeyStore()
 const newUserEmail = ref("")
-const users = ref<Array<UserInfo>>([])
-var groceryList = new GroceryList()
 
-const listener = attachEvent("GroceryLists/" + groceryListKeyStore.key, (snapshot) => {
-    groceryList = GroceryList.fromObject(snapshot)
-    var tempUsers: Array<UserInfo> = []
+const groceryList = getList(groceryListKeyStore.key)
+var usersOfList = computed(() => getUsersOnListToDisplay())
+var recommendedUsers = computed(() => getRecommendedsToDisplay())
+
+function getUsersOnListToDisplay() {
+    var people = getAllPeopleOnList(groceryListKeyStore.key)
+    return convertUserEmailsToUserInfos(people)
+}
+
+function getRecommendedsToDisplay() {
+    var allMyLists = getAUsersLists(authStore.userEmail);
+
+    let usersOfListEmails = usersOfList.value.flatMap((user) => user.Email)
+    let history = allMyLists.flatMap(([, list]) => list.Users)
     
-    for (var email of groceryList.Users) {
-        var userInfo = getUserInfo(email) ?? new UserInfo("", email)
-        tempUsers.push(userInfo)
+    let usersToRecommend: string[] = [];
+    
+    for(let email of history) {
+        if(!usersOfListEmails.includes(email) && !usersToRecommend.includes(email) && authStore.userEmail !== email) {
+            usersToRecommend.push(email);
+        }
     }
 
-    users.value = tempUsers
-});
+    return convertUserEmailsToUserInfos(usersToRecommend);
+}
   
 function addUser() {
     var email = newUserEmail.value.trim()
+
     if (email.length > 0) {
         var userInfo = getUserInfo(email)
         if (userInfo == null) {
-            quasar.notify({color: 'red', position: 'center', message: "No user found with email '" + email + "'!"})
+            usePopupStore().displayPopup({color: 'red', position: 'center', message: "No user found with email '" + email + "'!"})
+            return
+        }
+
+        groceryList.addUser(userInfo.Email)
+        newUserEmail.value = ""
+        saveGroceryList()
+    }
+}
+
+function addRecommendedUser(email: string) {
+    if (email.length > 0) {
+        var userInfo = getUserInfo(email)
+        if (userInfo == null) {
+            usePopupStore().displayPopup({color: 'red', position: 'center', message: "No user found with email '" + email + "'!"})
             return
         }
 
@@ -76,7 +118,7 @@ function addUser() {
 }
 
 function tryLeaveList() {
-    quasar.notify({color: 'red', position: 'center', message: "Are you sure you want to leave the list '" + groceryList.Name + "'?", actions: [{label: 'Yes', color: 'white', handler: () => { leaveList() }}, {label: 'No', color: 'white'}]})
+    usePopupStore().displayPopup({color: 'red', position: 'center', message: "Are you sure you want to leave the list '" + groceryList.Name  + "'?", actions: [{label: 'Yes', color: 'white', handler: () => { leaveList() }}, {label: 'No', color: 'white'}]})
 }
 
 function leaveList() {
@@ -94,6 +136,17 @@ function leaveList() {
 
 function saveGroceryList() {
     updateDb("GroceryLists/" + groceryListKeyStore.key, groceryList)
+}
+
+function convertUserEmailsToUserInfos(users: string[]) {
+    var usersWithInfo: UserInfo[] = [];
+
+    for (var email of users) {
+        var userInfo = getUserInfo(email) ?? new UserInfo("", email)
+        usersWithInfo.push(userInfo)
+    }
+
+    return usersWithInfo;
 }
 
 </script>
