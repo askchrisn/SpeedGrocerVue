665844bf003536528b7b35764a144d73017af8fc
askchrisn <fundasappteam@gmail.com>
2024-01-05 02:33:53 -0500

    Remember Password

---
+++ src/components/ToggleWithLabel.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <label>{{ label }}</label>
+        <q-toggle v-model="inputValue" />
+    </div>
+</template>
+  
+<script setup lang="ts">
+import { watch } from 'vue';
+import { defineProps, defineEmits, ref } from 'vue';
+
+const props = defineProps({
+    modelValue: Boolean,
+    label: String,
+    onToggle: {
+        type: Function,
+        default: undefined
+    }
+});
+
+let inputValue = ref(props.modelValue);
+
+const emit = defineEmits(['update:modelValue', 'ontoggled']);
+
+watch(() => inputValue.value, () => {
+    emit('update:modelValue', inputValue.value);
+    emit('ontoggled');
+});
+
+</script>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -2,15 +2,16 @@
     <div class="main">
       <div class="flex-column flex-grow h-center">
         <q-input class="q-mb-lg" filled v-model="email" label="Email" stack-label></q-input>
-        <q-input class="q-mb-lg" v-model="password" filled :type="isPasswordHidden ? 'password' : 'text'" stack-label label="Password">
-        <template v-slot:append>
-          <q-icon
-            :name="isPasswordHidden ? 'visibility_off' : 'visibility'"
-            class="cursor-pointer"
-            @click="isPasswordHidden = !isPasswordHidden"
-          ></q-icon>
-        </template>
+        <q-input v-model="password" filled :type="isPasswordHidden ? 'password' : 'text'" stack-label label="Password">
+          <template v-slot:append>
+            <q-icon
+              :name="isPasswordHidden ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPasswordHidden = !isPasswordHidden"
+            ></q-icon>
+          </template>
         </q-input>
+        <ToggleWithLabel class="mb1" label="Remember Password?" v-model="rememberPassword" />
         <q-btn class="q-mb-lg" color="primary" @click="login">Login</q-btn>
         <div class="flex-row h-center">
             <label>Don't have an account?</label>
@@ -21,10 +22,11 @@
 </template>
 
 <script setup lang="ts">
-    import { ref } from 'vue';
+    import { Ref, ref } from 'vue';
     import { useRouter } from 'vue-router';
     import { useAuthStore } from 'src/stores/authStore';
-    import { Notify } from 'quasar'
+    import { LocalStorage, Notify } from 'quasar'
+    import ToggleWithLabel from 'src/components/ToggleWithLabel.vue';
 
     const router = useRouter();
     const authStore = useAuthStore();
@@ -32,15 +34,31 @@
     const email = ref('');
     const password = ref('');
     const isPasswordHidden = ref(true)
+    const rememberPassword = loadRememberSettings();
 
     async function login() {
         try {
-            await authStore.signIn(email.value, password.value);
+            saveOrForgetPassword()
+            await authStore.signIn(email.value, password.value)
             router.push('/');
         } catch (error: any) {
             Notify.create({ type: 'negative', message: error.message })
         }
     };
+
+    function loadRememberSettings(): Ref<boolean> {
+      var password = LocalStorage.getItem('rememberPassword')
+      return ref(!!password)
+    }
+
+    function saveOrForgetPassword() {
+      if (rememberPassword.value) {
+          LocalStorage.set('rememberPassword', password.value)
+      } else {
+          LocalStorage.remove('rememberPassword')
+      }
+    }
+    
 </script>
 
 <style scoped>
